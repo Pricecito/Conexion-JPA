eca08c91239520fcbafd1108eb95a9b5adf9b79b
Pricecito <matas.gutierrez.1254@gmail.com>
2024-12-10 16:53:25 -0500

    ManyToMany Unidireccional

--- src/main/java/com/example/persistenceapp/model/Articulo.java
+++ src/main/java/com/example/persistenceapp/model/Articulo.java
@@ -10,6 +10,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -32,6 +35,13 @@ public class Articulo {
 
     @OneToMany(mappedBy = "articulo", cascade = CascadeType.ALL)
     private List<DetalleFactura> detalles = new ArrayList<DetalleFactura>();
+
+    // con esto decimos que no se eliminen las categorias si eliminamos un articulo
+    // o que se actualicen
+    // con ManyToMany tenemos ue crear una tabla con los id de ambas entidades
+    @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @JoinTable(name = "articulo_categoria", joinColumns = @JoinColumn(name = "articulo_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+    private List<Categoria> categorias = new ArrayList<Categoria>();
 
     public Long getId() {
         return id;
